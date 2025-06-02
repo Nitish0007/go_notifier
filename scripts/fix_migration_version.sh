532efca848539a7bb96b194f8191194987266f8b
Nitish Sharma <0007nitishsharma@gmail.com>
2025-06-02 22:37:52 +0530

    added notification table and its partitions

---
+++ scripts/fix_migration_version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DB_URL="postgres://postgres:12345678@notifier_db:5432/notifier_dev_db?sslmode=disable"
+
+echo "Cleaning migration dirty state...."
+
+psql $DB_URL <<EOF
+UPDATE schema_migrations SET dirty = false WHERE dirty = true;
+EOF
+
+echo "Done, now you can run migrations again"
+
+
+# command for rollback
+
+# docker run --rm \
+#   --network go_notifier_default \
+#   -v $(pwd)/db/migrations:/migrations \
+#   migrate/migrate \
+#   -path=/migrations \
+#   -database "postgres://postgres:12345678@notifier_db:5432/notifier_dev_db?sslmode=disable" \
+#   force 2
